e52bf9b68b67ac89535cecdc13bed1c5cc028aaa
Zoe.exe <83599055+Kris6528@users.noreply.github.com>
2022-08-29 12:16:01 +0800

    update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -188,6 +188,10 @@ class StoryMenuState extends MusicBeatState
 
 		changeWeek();
 
+		#if android
+		addVirtualPad(LEFT_FULL, A_B_X_Y);
+		#end
+
 		super.create();
 	}
 
@@ -242,23 +246,28 @@ class StoryMenuState extends MusicBeatState
 			else if (upP || downP)
 				changeDifficulty();
 
-			if(FlxG.keys.justPressed.CONTROL)
+			if(FlxG.keys.justPressed.CONTROL #if android || virtualPad.buttonX.justPressed #end)
 			{
+				#if android
+				removeVirtualPad();
+				#end
 				persistentUpdate = false;
 				openSubState(new GameplayChangersSubstate());
 			}
-			else if(controls.RESET)
+			else if(controls.RESET #if android || virtualPad.buttonY.justPressed #end)
 			{
+				#if android
+				removeVirtualPad();
+				#end
 				persistentUpdate = false;
 				openSubState(new ResetScoreSubState('', curDifficulty, '', curWeek));
 				//FlxG.sound.play(Paths.sound('scrollMenu'));
 			}
 			else if (controls.ACCEPT)
 			{
-				FlxTween.tween(loadscreen, {alpha: 1}, 1.0, {ease: FlxEase.expoInOut});
 				selectWeek();
+				FlxTween.tween(loadscreen, {alpha: 1}, 1.0, {ease: FlxEase.expoInOut});
 			}
-		}
 
 		if (controls.BACK && !movedBack && !selectedWeek)
 		{
