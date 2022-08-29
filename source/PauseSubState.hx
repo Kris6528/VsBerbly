28f3d3d952ecfc313d552b565d6521296533a877
Zoe.exe <83599055+Kris6528@users.noreply.github.com>
2022-08-29 12:02:51 +0800

    update PauseSubState.hx

--- source/PauseSubState.hx
+++ source/PauseSubState.hx
@@ -116,6 +116,19 @@ class PauseSubState extends MusicBeatSubstate
 		changeSelection();
 
 		cameras = [FlxG.cameras.list[FlxG.cameras.list.length - 1]];
+
+		#if android
+		if (PlayState.chartingMode)
+		{
+			addVirtualPad(LEFT_FULL, A);
+		}
+		else
+		{
+			addVirtualPad(UP_DOWN, A);
+		}
+		addPadCamera();
+		#end
+
 	}
 
 	override function update(elapsed:Float)
